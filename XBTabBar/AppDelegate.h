8d8155e5790d0295f5ce4e60512aab41e39a7232
xissburg <xissburg@gmail.com>
2012-02-05 22:52:59 -0200

    Initial Commit

---
+++ XBTabBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XBTabBar
+//
+//  Created by xiss burg on 2/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
